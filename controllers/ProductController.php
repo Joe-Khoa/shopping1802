36d54cad4f31085c90f88d6ff59e6578962b742d
Huong <huongnguyenak96@gmail.com>
2019-04-22 20:41:37 +0700

    controller

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,9 +1,30 @@
 <?php
 require_once 'Controller.php';
+require_once "models/ProductModel.php";
 
 class ProductController extends Controller{
     function loadProduct(){
-        return parent::loadView('product');
+        if(isset($_GET['url']) && $_GET['id']){
+            $url = $_GET['url']; //iphone-x-64gb-gray
+            $id = $_GET['id']; // 105
+            $model = new ProductModel;
+            $product = $model->getProductDetail($url,$id);
+            if($product){
+                //load view
+                $title = $product->name;
+                $data = [
+                    'product'=>$product,
+                    
+                ];
+                return parent::loadView('product',$title,$data);
+            }
+            else{
+                header('Location: 404.error.php');
+            }
+        }
+        else{
+            header('Location: 404.error.php');
+        }
     }
 }
 
--- models/ProductModel.php
+++ models/ProductModel.php
@@ -1,20 +1,21 @@
 <?php
 require_once 'DBConnect.php';
+
 class ProductModel extends DBConnect{
     function getProductDetail($url, $id){
         // $sql = "SELECT p.*
         //         FROM products p
         //         INNER JOIN page_url u
         //         ON p.id_url = u.id
-        //         WHERE page_url.url = '$url'
+        //         WHERE u.url = '$url'
         //         AND p.id = $id";
         // return $this->getOneRow($sql);
-        
+
         $sql = "SELECT p.*
                 FROM products p
                 INNER JOIN page_url u
                 ON p.id_url = u.id
-                WHERE page_url.url = ?
+                WHERE u.url = ?
                 AND p.id = ?";
         return $this->getOneRow($sql,[$url,$id]);
     }
