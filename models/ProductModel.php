71ac4373f12fd4514d6a0aa4227a95d68ef44028
Huong <huongnguyenak96@gmail.com>
2019-04-22 20:11:06 +0700

    getProductDetail

---
+++ models/ProductModel.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'DBConnect.php';
+class ProductModel extends DBConnect{
+    function getProductDetail($url, $id){
+        // $sql = "SELECT p.*
+        //         FROM products p
+        //         INNER JOIN page_url u
+        //         ON p.id_url = u.id
+        //         WHERE page_url.url = '$url'
+        //         AND p.id = $id";
+        // return $this->getOneRow($sql);
+        
+        $sql = "SELECT p.*
+                FROM products p
+                INNER JOIN page_url u
+                ON p.id_url = u.id
+                WHERE page_url.url = ?
+                AND p.id = ?";
+        return $this->getOneRow($sql,[$url,$id]);
+    }
+}
+
+
+
+?>
