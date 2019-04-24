f0810e732301e7b8bc37d4dad037f8ebbfb9c108
Huong <huongnguyenak96@gmail.com>
2019-04-24 20:03:43 +0700

    ProductTypeModel

---
+++ models/ProductTypeModel.php
@@ -0,0 +1,17 @@
+<?php
+require 'DBConnect.php';
+
+class ProductTypeModel extends DBConnect{
+
+    //ALTER TABLE `categories` ADD `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1_show, 0_hide' AFTER `icon`;
+    function getAllCategories(){
+        $sql = "SELECT c.name, c.icon, u.url
+                FROM categories c
+                INNER JOIN page_url u
+                ON c.id_url = p.id
+                WHERE c.status=1";// 1: show
+        return $this->getMoreRow($sql);
+    }
+}
+
+?>
