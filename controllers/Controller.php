33dc85509b2d29a857f9e3a0205e035efffbc122
Huong <huongnguyenak96@gmail.com>
2019-04-24 20:43:23 +0700

    product-type.php

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -1,8 +1,13 @@
 <?php
+require_once 'models/ProductTypeModel.php';
 
 class Controller{
+
     public function loadView(string $view="index", string $title='Home', array $data=[]){
+        $model = new ProductTypeModel();
+        $categories = $model->getAllCategories();
         require_once "views/master.view.php";
+
     }
 }
 
--- models/ProductTypeModel.php
+++ models/ProductTypeModel.php
@@ -3,12 +3,12 @@ require 'DBConnect.php';
 
 class ProductTypeModel extends DBConnect{
 
-    //ALTER TABLE `categories` ADD `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1_show, 0_hide' AFTER `icon`;
+    // ALTER TABLE `categories` ADD `status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1_show, 0_hide' AFTER `icon`;
     function getAllCategories(){
         $sql = "SELECT c.name, c.icon, u.url
                 FROM categories c
                 INNER JOIN page_url u
-                ON c.id_url = p.id
+                ON c.id_url = u.id
                 WHERE c.status=1";// 1: show
         return $this->getMoreRow($sql);
     }
