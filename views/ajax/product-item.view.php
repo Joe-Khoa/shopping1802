48902c810294bda58f789a4f1a0b1d14812fc5a8
Huong <huongnguyenak96@gmail.com>
2019-05-08 20:24:31 +0700

    img

--- views/ajax/product-item.view.php
+++ views/ajax/product-item.view.php
@@ -5,10 +5,10 @@
             <div class="product-thumbnail">
 
                 <div class="pr-img-area">
-                    <a title="Apple Cáp chuyển đổi Lightning to 3.5mm" href="apple-cap-chuyen-doi-lightning-to-35mm-31.html">
+                    <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html">
                         <figure>
-                            <img height="200px" class="first-img" src="public/products/apple-cap-chuyen-doi-lightning-to-35mm.png" alt="Apple Cáp chuyển đổi Lightning to 3.5mm">
-                            <img height="200px" class="hover-img" src="public/products/apple-cap-chuyen-doi-lightning-to-35mm.png" alt="Apple Cáp chuyển đổi Lightning to 3.5mm">
+                            <img height="200px" class="first-img" src="public/products/<?=$p->image?>" alt="<?=$p->name?>">
+                            <img height="200px" class="hover-img" src="public/products/<?=$p->image?>" alt="<?=$p->name?>">
                         </figure>
                     </a>
                     <button type="button" class="add-to-cart-mt">
@@ -20,18 +20,16 @@
             <div class="item-info">
                 <div class="info-inner">
                     <div class="item-title">
-                        <a title="Apple Cáp chuyển đổi Lightning to 3.5mm" href="apple-cap-chuyen-doi-lightning-to-35mm-31.html">
-                            Apple Cáp chuyển đổi Lightning to 3.5mm </a>
+                        <a title="<?=$p->name?>" href="<?=$p->url?>-<?=$p->id?>.html"><?=$p->name?> </a>
                     </div>
                     <div class="item-content">
                         <div class="item-price">
                             <div class="price-box">
                                 <p class="special-price">
                                     <span class="price">
-                                        299,000 vnd
+                                    <?=number_format($p->price)?> vnd
                                     </span>
                                 </p>
-
                             </div>
                         </div>
                     </div>
