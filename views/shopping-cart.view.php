4c2d7406bc7747fe8c56f65360adc08f144d7cbd
Huong <huongnguyenak96@gmail.com>
2019-05-15 19:55:05 +0700

    check cart empty

--- views/shopping-cart.view.php
+++ views/shopping-cart.view.php
@@ -9,66 +9,74 @@
             <h2>Shopping Cart</h2>
           </div>
             
-            
             <div class="order-detail-content">
-              <div class="table-responsive">
-                <table class="table table-bordered cart_summary">
-                  <thead>
-                    <tr>
-                      <th class="cart_product">Product</th>
-                      <th>Name</th>
-                      <th>Unit price</th>
-                      <th>Qty</th>
-                      <th>Total</th>
-                      <th  class="action"><i class="fa fa-trash-o"></i></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php 
-                    foreach($data['cart']->items as $product):?>
-                    <tr>
-                      <td class="cart_product"><a href="#"><img src="public/products/<?=$product['item']->image?>" alt="Product"></a></td>
-                      <td class="cart_description"><p class="product-name"><a href="#">
-                        <?=$product['item']->name?>
-                      </a></p>
-                      <td class="price"><span>
-                        <?php
-                          if($product['item']->price == $product['item']->promotion_price)
-                          echo number_format($product['item']->price);
-                          else 
-                          {
-                            echo '<del style="color:#000" >'.number_format($product['item']->price).'</del>';
-                            echo '<br>';
-                            echo number_format($product['item']->promotion_price);
-                          }
-                        ?>
-                      </span></td>
-                      <td class="qty"><input class="form-control input-sm" type="text" value="<?=$product['qty']?>"></td>
-                      <td class="price"><span>
-                        <?=number_format($product['promotionPrice'])?>
-                      </span></td>
-                      <td class="action"><a href="#"><i class="icon-close"></i></a></td>
-                    </tr>
-                    <?php endforeach?>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <td colspan="2" rowspan="3"></td>
-                      <td colspan="3">Price</td>
-                      <td colspan="2"><?=number_format($data['cart']->totalPrice)?></td>
-                    </tr>
-                    <tr>
-                      <td colspan="3">Off</td>
-                      <td colspan="2">-<?=number_format($data['cart']->totalPrice-$data['cart']->promtPrice)?></td>
-                    </tr>
-                    <tr>
-                      <td colspan="3"><strong>Promotion price</strong></td>
-                      <td colspan="2"><strong><?=number_format($data['cart']->promtPrice)?></strong></td>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <div class="cart_navigation"> <a class="continue-btn" href="#"><i class="fa fa-arrow-left"> </i>&nbsp; Continue shopping</a> <a class="checkout-btn" href="#"><i class="fa fa-check"></i> Proceed to checkout</a> </div>
+              <?php if($data['cart']->totalQty==0):?>
+                <p class="text-center">Giỏ hàng rỗng, vui lòng mua hàng</p>
+                <div class="cart_navigation "> <a class="continue-btn" href="#"><i class="fa fa-arrow-left"> </i>&nbsp; Tiếp tục mua sắm</a>
+
+              <?php else:  ?>
+                <div class="table-responsive">
+                  <table class="table table-bordered cart_summary">
+                    <thead>
+                      <tr>
+                        <th class="cart_product">Product</th>
+                        <th>Name</th>
+                        <th>Unit price</th>
+                        <th>Qty</th>
+                        <th>Total</th>
+                        <th  class="action"><i class="fa fa-trash-o"></i></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <?php 
+                      foreach($data['cart']->items as $product):?>
+                      <tr>
+                        <td class="cart_product"><a href="#"><img src="public/products/<?=$product['item']->image?>" alt="Product"></a></td>
+                        <td class="cart_description"><p class="product-name"><a href="#">
+                          <?=$product['item']->name?>
+                        </a></p>
+                        <td class="price"><span>
+                          <?php
+                            if($product['item']->price == $product['item']->promotion_price)
+                            echo number_format($product['item']->price);
+                            else 
+                            {
+                              echo '<del style="color:#000" >'.number_format($product['item']->price).'</del>';
+                              echo '<br>';
+                              echo number_format($product['item']->promotion_price);
+                            }
+                          ?>
+                        </span></td>
+                        <td class="qty"><input class="form-control input-sm" type="text" value="<?=$product['qty']?>"></td>
+                        <td class="price"><span>
+                          <?=number_format($product['promotionPrice'])?>
+                        </span></td>
+                        <td class="action"><a href="#"><i class="icon-close"></i></a></td>
+                      </tr>
+                      <?php endforeach?>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        <td colspan="2" rowspan="3"></td>
+                        <td colspan="3">Price</td>
+                        <td colspan="2"><?=number_format($data['cart']->totalPrice)?></td>
+                      </tr>
+                      <tr>
+                        <td colspan="3">Off</td>
+                        <td colspan="2"><?=number_format($data['cart']->promtPrice-$data['cart']->totalPrice)?></td>
+                      </tr>
+                      <tr>
+                        <td colspan="3"><strong>Promotion price</strong></td>
+                        <td colspan="2"><strong><?=number_format($data['cart']->promtPrice)?></strong></td>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
+              
+                <div class="cart_navigation"> <a class="continue-btn" href="#"><i class="fa fa-arrow-left"> </i>&nbsp; Tiếp tục mua sắm</a> 
+                <a class="checkout-btn" href="#"><i class="fa fa-check"></i> Thanh toán</a> 
+              <?php endif?>
+            </div>
             </div>
           </div>
         </div>
